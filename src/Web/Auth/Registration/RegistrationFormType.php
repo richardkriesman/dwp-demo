8661b8b5170cba7ea1b4bfc20c04d7acdf4e5fde
Richard Kriesman <rkriesman@gmail.com>
2020-10-16 23:04:21 -0500

    Styled registration and login forms.

--- src/Web/Auth/Registration/RegistrationFormType.php
+++ src/Web/Auth/Registration/RegistrationFormType.php
@@ -5,6 +5,7 @@ namespace App\Web\Auth\Registration;
 use App\Domain\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -47,6 +48,7 @@ final class RegistrationFormType extends AbstractType
                     ]),
                 ],
             ])
+            ->add('submit', SubmitType::class);
         ;
     }
 
